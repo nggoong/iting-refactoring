b472fd8f9013ba6175898900d8ed7ab02813b3b4
nggoong <nggoong@naver.com>
2023-01-17 19:08:08 +0900

    refactor: js에서 ts파일로 변환

--- src/components/hashtag/NomalBadge.tsx
+++ src/components/hashtag/NomalBadge.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
 
-const NomalBadge = ({ children }) => {
+interface Props {
+	children: ReactNode;
+}
+
+const NomalBadge = ({ children }: Props) => {
 	return (
 		<Stack direction="row" spacing={1}>
 			<Chip label={children} sx={{ background: '#3549FF', color: 'white', fontSize: '12px' }} />
