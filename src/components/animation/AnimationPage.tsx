a7d83e375f0e8bb6168f497a01f3a2f7fa29281f
nggoong <nggoong@naver.com>
2023-01-17 19:16:36 +0900

    refactor: js에서 ts파일로 변환

--- src/components/animation/AnimationPage.tsx
+++ src/components/animation/AnimationPage.tsx
@@ -6,7 +6,7 @@ const animations = {
 	exit: { opacity: 0 }
 };
 
-const AnimationPage = ({ children }) => {
+const AnimationPage = ({ children }: React.PropsWithChildren) => {
 	return (
 		<motion.div variants={animations} initial="initial" exit="exit" transition={{ duration: 0.2 }}>
 			{children}
