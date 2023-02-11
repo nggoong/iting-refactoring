e3ac0d06fef2c00bbd3f37f9a9127162567145ca
nggoong <nggoong@naver.com>
2023-02-11 16:18:05 +0900

    refactor: StompJS 사용을 위한 커스텀 훅 정의, 모듈화
    커스텀 훅 useSock을 정의하여 StompJS 기능 모듈화

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
-import SockJS from 'sockjs-client';
-import * as StompJS from 'stompjs';
-import { chatroomAPI } from '../shared/api';
 import { Helmet } from 'react-helmet';
 import SubmitForm from '../components/submitForm/SubmitForm';
 import MyBubble from '../components/speechBubble/MyBubble';
@@ -11,7 +8,8 @@ import OtherBubble from '../components/speechBubble/OtherBubble';
 import Header from '../components/header/Header';
 import Notice from '../components/speechBubble/Notice';
 import { TypeChatMessage, TypeChatRoomNavigateState } from '../typings';
-import useExitRoomMutation from '../hooks/queries/useExitRoomMutation';
+import { useQueryClient } from '@tanstack/react-query';
+import useSock from '../hooks/socket/useSock';
 
 const ChatRoom = () => {
 	const [messages, setMessages] = useState<TypeChatMessage[]>([]);
@@ -22,115 +20,40 @@ const ChatRoom = () => {
 	const navigate = useNavigate();
 	const { state: navigateState } = useLocation() as TypeChatRoomNavigateState;
 	const roomId = Number(useParams().roomId);
-
-	const sock = new SockJS(`${process.env.REACT_APP_API_URL}/iting`);
-	const client = StompJS.over(sock);
-	client.debug = null;
 	const headers = {};
-
-	const { mutate: exitRoom } = useExitRoomMutation();
-
-	const disConnect = () => {
-		client.send(
-			`/api/pub/${roomId}`,
-			headers,
-			JSON.stringify({
-				enter: 'EXIT',
-				messageType: 'TEXT',
-				nickname: nicknameRef.current,
-				message: `${nicknameRef.current}님이 퇴장하였습니다.`
-			})
-		);
-		client.disconnect(() => {
-			exitRoom(roomId);
-		}, {});
-	};
-	const socketReConnectFunc = (stompClient: StompJS.Client, callback: () => void) => {
-		setTimeout(() => {
-			/** Stomp.client에는 ws.readyState라는 integer 값이 있는데, 소켓 연결이 완료되었을 경우 이 값은 1로 설정됨 */
-			/** 연결이 완료 되었으면 callback을 실행함 */
-			if (stompClient.ws.readyState === 1) {
-				callback();
-			} else {
-				/** 연결이 되지 않았을 경우 다시 실행 */
-				socketReConnectFunc(stompClient, callback);
-			}
-		}, 1);
-	};
-
-	const sendMsg = (messageText: string) => {
-		const sendMessage = {
-			enter: 'COMM',
-			messageType: 'TEXT',
-			nickname: nicknameRef.current,
-			message: messageText
-		};
-		socketReConnectFunc(client, () => {
-			client.send(`/api/pub/${roomId}`, headers, JSON.stringify(sendMessage));
-		});
-	};
+	const { sockConnect, sendMsg, disConnect } = useSock();
+	const queryClient = useQueryClient();
 
 	const scrollToBottom = () => {
 		chatRef.current!.scrollIntoView({ behavior: 'smooth' });
 	};
 
 	useEffect(() => {
-		client.connect(
-			headers,
-			async () => {
-				client.subscribe(`/api/sub/${roomId}`, (data) => {
-					const newMessage = JSON.parse(data.body);
-					setMessages((prev) => [...prev, newMessage]);
-				});
-
-				try {
-					const res = await chatroomAPI.enterRoom(roomId);
-					nicknameRef.current = res.data;
-					client.send(
-						`/api/pub/${roomId}`,
-						headers,
-						JSON.stringify({
-							enter: 'ENTER',
-							messageType: 'TEXT',
-							nickname: nicknameRef.current
-						})
-					);
-				} catch (err) {
-					alert('방이 꽉 찼습니다!');
-					navigate('/viewer/room');
-					return;
-				}
-			},
-			() => {
-				alert('채팅방 연결 실패!');
-				navigate('/viewer/room');
-			}
-		);
+		sockConnect(roomId, headers, setMessages, nicknameRef);
 
 		return () => {
 			try {
 				if (!nicknameRef.current) return;
-				disConnect();
+				disConnect(roomId, headers, nicknameRef.current);
 			} catch (e) {
 				navigate('/viewer/room');
 			}
 		};
 	}, []);
 
-	const reloadFunction = () => {
-		disConnect();
-		navigate('/viewer/room');
+	const disConnectFunction = () => {
+		disConnect(roomId, headers, nicknameRef.current);
 	};
 
 	useEffect(() => {
-		window.addEventListener('beforeunload', reloadFunction);
-		window.addEventListener('unload', disConnect); // 브라우저를 닫았을 때
-		window.addEventListener('pagehide', disConnect);
+		window.addEventListener('beforeunload', disConnectFunction);
+		window.addEventListener('unload', disConnectFunction);
+		window.addEventListener('pagehide', disConnectFunction);
 
 		return () => {
-			window.removeEventListener('beforeunload', reloadFunction);
-			window.removeEventListener('unload', disConnect);
-			window.removeEventListener('pagehide', disConnect);
+			window.removeEventListener('beforeunload', disConnectFunction);
+			window.removeEventListener('unload', disConnectFunction);
+			window.removeEventListener('pagehide', disConnectFunction);
 		};
 	}, []);
 
@@ -141,10 +64,17 @@ const ChatRoom = () => {
 		if (messages[msglen - 1]?.enter === 'EXIT') {
 			if (!navigateState.isHost) {
 				alert('호스트가 퇴장하였습니다.');
+				(async () => {
+					await queryClient.invalidateQueries(['rooms']);
+				})();
 				navigate('/viewer/room');
 			}
 		}
 	}, [messages]);
+
+	const exeSendMsg = (text: string) => {
+		sendMsg(roomId, 'COMM', 'TEXT', text, headers, nicknameRef.current);
+	};
 
 	return (
 		<ChatRoomWrapper ref={tempRef}>
@@ -171,7 +101,7 @@ const ChatRoom = () => {
 				)}
 				<div ref={chatRef} style={{ height: '10px' }}></div>
 			</ChatArea>
-			<SubmitForm sendMsg={sendMsg} />
+			<SubmitForm sendMsg={exeSendMsg} />
 		</ChatRoomWrapper>
 	);
 };
